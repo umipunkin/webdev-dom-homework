77c93467772889f975f3e9196a55547fec6c1b67
umipunkin <teddmandarinov@gmail.com>
2025-06-01 21:53:53 +0300

    Выполнил ДЗ по API

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+import { setCommentsData } from "./commentsData.js";
 import {renderComments} from "./renderComments.js";
 renderComments();
+
+fetch('https://wedev-api.sky.pro/api/v1/umipunkin/comments').then(response => response.json()).then(data => {
+    setCommentsData(data.comments)
+    renderComments();
+})
+
--- initListeners.js
+++ initListeners.js
@@ -1,11 +1,11 @@
 import { renderComments } from "./renderComments.js";
-import { commentsData } from "./comentsData.js";
+import { commentsData } from "./commentsData.js";
 
 export const toggleLike = (commentId) => {
     const comment = commentsData.find((c) => c.id === commentId)
     if (comment) {
         comment.isLiked = !comment.isLiked
-        comment.likesCount += comment.isLiked ? 1 : -1
+        comment.likes += comment.isLiked ? 1 : -1
         renderComments()
     }
 }
@@ -13,7 +13,7 @@ export const toggleLike = (commentId) => {
 export const setCurrentComment = (currentId) => {
     const comment = commentsData.find(({ id }) => id === currentId)
 
-    const quoteElement = `"${comment.name} \n ${comment.text}"\n\n`
+    const quoteElement = `"${comment.author.name} \n ${comment.text}"\n\n`
     document.getElementById('input').value = ''
     document.getElementById('textArea').value = quoteElement
 }

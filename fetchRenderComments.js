34dd143e22edbfdb59f40e0a02db2d096ad8fcd2
umipunkin <teddmandarinov@gmail.com>
2025-06-13 16:15:26 +0300

    Внес корректировки согласно замечаниям

--- fetchRenderComments.js
+++ fetchRenderComments.js
@@ -2,8 +2,17 @@ import { setCommentsData } from "./commentsData.js";
 import { renderComments } from "./renderComments.js";
 
 export const fetchRenderComments = () => {
+
+    document.getElementById('loader').style.display = 'none';
+    
+
     return fetch('https://wedev-api.sky.pro/api/v1/umipunkin/comments').then(response => response.json()).then(data => {
+
+        
+        
         setCommentsData(data.comments)
         renderComments();
+
+    
     })
 }
--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 
 import { fetchRenderComments } from "./fetchRenderComments.js";
-import {renderComments} from "./renderComments.js";
 
-window.addEventListener('load', function() {
-    setTimeout(function() {
-        document.getElementById('loader').style.display = 'none';
-        document.getElementById('content').style.display = 'block';
-    }, 2000);
-});
+document.getElementById('loader').style.display = 'block';
 
-renderComments();
 
 fetchRenderComments();
 

9b8b6a994df4fdca68bef673256851a0cd0319f0
umipunkin <teddmandarinov@gmail.com>
2025-06-19 20:23:33 +0300

    Внес корректировки согласо замечаниям

--- api.js
+++ api.js
@@ -1,7 +1,10 @@
- export const fetchComments = () => fetch('https://wedev-api.sky.pro/api/v1/umipunkin/comments')
+ const apiLink = 'https://wedev-api.sky.pro/api/v1/umipunkin/comments'
+ 
+ 
+ export const fetchComments = () => fetch(apiLink)
 
 
- export const createComment = (comment) => fetch('https://wedev-api.sky.pro/api/v1/umipunkin/comments', {
+ export const createComment = (comment) => fetch(apiLink, {
     method: 'POST',
     body: JSON.stringify(comment)
  } )
